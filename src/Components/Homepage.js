5b430a5ef777ecc12a400aa385d859c8b8cc67af
fatemehalimardaani <fatemeh.alimardaani@gmail.com>
2019-11-01 23:47:39 +0330

    setting up homepage

---
+++ src/Components/Homepage.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import '../Syles/homepage.scss'
+export const Homepage=()=>{
+
+    return(
+        <div className='home-page'>
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">
+                            HATS
+                        </h1>
+                        <span className="subtitle">
+                            SHOP NOW
+                        </span>
+                    </div>
+                </div>
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">
+                            HATS
+                        </h1>
+                        <span className="subtitle">
+                            SHOP NOW
+                        </span>
+                    </div>
+                </div>
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">
+                            HATS
+                        </h1>
+                        <span className="subtitle">
+                            SHOP NOW
+                        </span>
+                    </div>
+                </div>
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">
+                            HATS
+                        </h1>
+                        <span className="subtitle">
+                            SHOP NOW
+                        </span>
+                    </div>
+                </div>
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">
+                            HATS
+                        </h1>
+                        <span className="subtitle">
+                            SHOP NOW
+                        </span>
+                    </div>
+                </div>
+
+
+            </div>
+
+        </div>
+    )
+
+}
