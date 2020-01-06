5afaa9473d936ee71f3a2fd5eeac79d4c44747bc
Fatemeh <fatemeh@Fatemehs-MacBook-Air.local>
2020-01-07 01:35:17 +0330

    finishing sign in handle submit

--- src/Components/sign-in/sign-in.js
+++ src/Components/sign-in/sign-in.js
@@ -2,7 +2,7 @@ import React from 'react';
 import FormInput from './../form-input/form-input'
 import './sign-in.scss';
 import CustomButton from './../../Components/custom-button/custom-btn';
-import {signInWithGoogle} from "../../firebase/firebase.utils";
+import {auth,signInWithGoogle} from "../../firebase/firebase.utils";
 
 class SignIn extends React.Component{
     constructor(props) {
@@ -13,8 +13,26 @@ class SignIn extends React.Component{
 
         }
     }
-    handleSubmit=(event)=>{
+    handleSubmit=async event=>{
         event.preventDefault();
+        const {email,password}=this.state;
+
+
+        try{
+
+            await auth.signInWithEmailAndPassword(email,password);
+            this.setState({
+                email:'',
+                password:''
+            })
+
+        }catch(e){
+            console.log(e)
+            alert(e.message)
+
+        }
+
+        
         this.setState({email:'',password:''})
     };
     handleChange=(event)=>{
