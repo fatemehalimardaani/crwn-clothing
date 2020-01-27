ce2012d210dfaab00b375613400a5abe576412f6
Fatemeh <fatemeh@Fatemehs-MacBook-Air.local>
2020-01-27 17:06:26 +0330

    added memoized reselector replaced cart selector

--- src/Components/cart-component/cartDropdown-component.js
+++ src/Components/cart-component/cartDropdown-component.js
@@ -3,6 +3,7 @@ import './cartDropdown-component.scss';
 import CustomButton from './../custom-button/custom-btn';
 import CartItem from './cart-item-component';
 import {connect} from 'react-redux';
+import {selectCartItems} from './../../redux/cart/cart.selector'
 const CartDropdown =({cartItems})=>(
     <div className="cart-dropdown">
         <div className="cart-items">
@@ -23,7 +24,7 @@ const CartDropdown =({cartItems})=>(
 )
 
 const mapStateToProps=(state)=>({
-    cartItems:state.cart.cartItems
+    cartItems:selectCartItems(state)
 
 })
 export default connect(mapStateToProps)(CartDropdown);
--- src/Components/cart-component/cartIcon-component.js
+++ src/Components/cart-component/cartIcon-component.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cartIcon-component.scss';
-
 import {connect} from 'react-redux';
-import {toggleCartHidden} from './../../redux/cart/cart.action'
-const CartIcon =( {toggleCartHidden} )=>(
+import {toggleCartHidden} from './../../redux/cart/cart.action';
+import {selectCartItemsCount} from './../../redux/cart/cart.selector'
+const CartIcon =( {toggleCartHidden,itemCount} )=>(
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className="shopping-icon"/>
         <span className="item-count">
-            0
+            {itemCount}
         </span>
 
     </div>
 )
 
+
+
+
+const mapStateToProps= state =>{
+    
+    console.log('Iam called')
+    return({
+            itemCount:selectCartItemsCount(state)
+         })
+    
+}
 const mapDispatchToProps=dispatch=>({
 
     toggleCartHidden: () => dispatch(toggleCartHidden())
@@ -21,4 +32,4 @@ const mapDispatchToProps=dispatch=>({
 
 })
 
-export default connect(null,mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps,mapDispatchToProps)(CartIcon);
